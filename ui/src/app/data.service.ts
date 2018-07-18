05472254258eed642973c77fca7776df32cf50b1
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-07-18 13:29:02 +0200

    added snackbabr

--- ui/src/app/data.service.ts
+++ ui/src/app/data.service.ts
@@ -6,6 +6,8 @@ import { User } from './models/user.model';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
+import {MatSnackBar} from '@angular/material';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,7 +17,7 @@ export class DataService {
   // private readonly API_URL = 'https://composer-rest-server-vehicle-manufacture-netw-1531824951915.mybluemix.net/api/Mortgage';
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public snackBar: MatSnackBar) { }
   
   getUser(): Observable<User[]> {
     return this.http.get<User[]>("https://jsonplaceholder.typicode.com/users");
@@ -29,12 +31,18 @@ export class DataService {
     return this.http.post("http://composer-rest-server-vehicle-manufacture-netw-1531824951915.mybluemix.net/api/mortgage", mortgage).subscribe(
       res => {
         console.log(res);
+        this.snackBar.open("Mortgage added.", "Ok", {
+          duration: 500,
+        });
       },
       (err: HttpErrorResponse) => {
         console.log(err.error);
         console.log(err.name);
         console.log(err.message);
         console.log(err.status);
+        this.snackBar.open("Sorry, something went wrong :-(", "Ok", {
+          duration: 500,
+        });
       }
     );
   }
@@ -43,12 +51,18 @@ export class DataService {
     return this.http.post("https://composer-rest-server-vehicle-manufacture-netw-1531824951915.mybluemix.net/api/CDO", cdo).subscribe(
       res => {
         console.log(res);
+        this.snackBar.open("CDO created", "Ok", {
+          duration: 500,
+        });
       },
       (err: HttpErrorResponse) => {
         console.log(err.error);
         console.log(err.name);
         console.log(err.message);
         console.log(err.status);
+        this.snackBar.open("Sorry, something went wrong :-(", "Ok", {
+          duration: 500,
+        });
       }
     );
   }
