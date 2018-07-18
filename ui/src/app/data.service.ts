ea3127b107e1bc6d1088ecc090bdb2b2d41b3d8e
Arthur Liebhardt <liebhardt.arthur@gmail.com>
2018-07-18 16:40:49 +0200

    removed user

--- ui/src/app/data.service.ts
+++ ui/src/app/data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
-import { User } from './models/user.model';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
